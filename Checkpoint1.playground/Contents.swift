32e5581a5781da18300393cb048f71cb791e51d9
Salvatori, Tamara <tamarasalvatori@gmail.com>
2023-10-31 08:03:09 -0300

    First week of 100 Days of Swift

---
+++ Checkpoint1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let temperatureInCelsius = 12.4
+
+var temperatureInFarenheit = ((temperatureInCelsius * 9)/5) + 32
+
+print("The temperature in Celsius is \(temperatureInCelsius) °C, and the temperature in Farenheit is \(temperatureInFarenheit) °F")
---
+++ Checkpoint2.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let catNames = ["Dolly", "Darf", "Tches", "Sessi", "Garfield", "Brenda", "Dolly", "Kroegger", "Roni", "Shaki", "Kira", "Tatá", "Flora", "Laura"]
+
+print(catNames.count)
+
+let catNameNotRepeated = Set(catNames)
+
+print(catNameNotRepeated.count)
+
+
